686161a73a5c54e062d4ba77b5674ab25f08fc23
qfdk <qfdk2010@gmail.com>
2021-05-20 20:45:50 +0200

    fix : 修改布局

--- app.js
+++ app.js
@@ -1,66 +1,71 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var WanNianLi = require('./lib/wuxing');
-var calendar = require('./lib/calendar');
-var LunarCalendar = require('lunar-calendar');
-var diPanDiZhiList = ["戊", "己", "庚", "辛", "壬", "癸", "丁", "丙", "乙"];
+const express = require('express');
+const app = express();
+const path = require('path');
+const WanNianLi = require('./lib/wuxing');
+const calendar = require('./lib/calendar');
+const diPanDiZhiList = ['戊', '己', '庚', '辛', '壬', '癸', '丁', '丙', '乙'];
 
-var xunShouList = {
-    "甲子": "戊",
-    "甲戊": "已",
-    "甲申": "庚",
-    "甲午": "辛",
-    "甲辰": "壬",
-    "甲寅": "癸"
-}
-var diPan = {
-    "6": "乾",
-    "1": "坎",
-    "8": "艮",
-    "3": "震",
-    "4": "巽",
-    "9": "离",
-    "2": "坤",
-    "7": "兑"
-}
-var men = {
-    "6": "开门",
-    "1": "休门",
-    "8": "生门",
-    "3": "伤门",
-    "4": "杜门",
-    "9": "景门",
-    "2": "死门",
-    "7": "惊门"
-}
+// 旬首
+const xunShouList = {
+    '甲子': '戊',
+    '甲戊': '已',
+    '甲申': '庚',
+    '甲午': '辛',
+    '甲辰': '壬',
+    '甲寅': '癸',
+};
 
-var xing = {
-    "6": "天心",
-    "1": "天蓬",
-    "8": "天任",
-    "3": "天冲",
-    "4": "天辅",
-    "9": "天英",
-    "2": "天芮",
-    "7": "天柱",
-    "5": "天禽"
-}
-var xingList = new Array(
-    "天心",
-    "天蓬",
-    "天任",
-    "天冲",
-    "天辅",
-    "天英",
-    "天芮",
-    "天柱"
-);
+// 地盘
+const diPan = {
+    '6': '乾',
+    '1': '坎',
+    '8': '艮',
+    '3': '震',
+    '4': '巽',
+    '9': '离',
+    '2': '坤',
+    '7': '兑',
+};
+
+// 八门
+const men = {
+    '6': '开门',
+    '1': '休门',
+    '8': '生门',
+    '3': '伤门',
+    '4': '杜门',
+    '9': '景门',
+    '2': '死门',
+    '7': '惊门',
+};
+// 九星
+const xing = {
+    '6': '天心',
+    '1': '天蓬',
+    '8': '天任',
+    '3': '天冲',
+    '4': '天辅',
+    '9': '天英',
+    '2': '天芮',
+    '7': '天柱',
+    '5': '天禽',
+};
+
+const xingList = [
+    '天心',
+    '天蓬',
+    '天任',
+    '天冲',
+    '天辅',
+    '天英',
+    '天芮',
+    '天柱',
+];
 
 // 现时
-var now;
+let now;
 
-var qimen = {}
+var qimen = {};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -69,45 +74,49 @@ app.engine('html', require('ejs').renderFile);
 // public files
 app.use(express.static(path.join(__dirname, 'public')));
 
+const getTimeDetail = (now) => {
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    const hour = now.getHours();
+    return {year, month, date, hour};
+};
+
 // index
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     now = new Date();
-    console.log(now.toLocaleString())
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var date = now.getDate();
-    var hour = now.getHours();
+    const {year, month, date, hour} = getTimeDetail(now);
     // 农历
-    var lunar = calendar.solar2lunar(year, month, date);
+    const lunar = calendar.solar2lunar(year, month, date);
     lunar.hour = hour;
+
     // 四柱
-    var bazi = WanNianLi.getResult(lunar).bazi;
+    const bazi = WanNianLi.getResult(lunar).bazi;
     // 甲子列表
-    var jiazi = WanNianLi.getResult(lunar).jiazi;
+    const jiazi = WanNianLi.getResult(lunar).jiazi;
     // 旬首
-    var xunShou = jiazi[jiazi.indexOf(bazi.hour) - (jiazi.indexOf(bazi.hour) % 10) + 1];
-    qimen["xunShou"] = xunShouList[xunShou];
-    qimen["shiGan"] = bazi.hour;
+    const xunShou = jiazi[jiazi.indexOf(bazi.hour) - (jiazi.indexOf(bazi.hour) % 10) + 1];
+    qimen['xunShou'] = xunShouList[xunShou];
+    qimen['shiGan'] = bazi.hour;
+    console.log(qimen);
+    console.log(bazi);
     res.render('index', {
         time: year + '/' + month + '/' + date + ' ' + hour + ':' + now.getMinutes(),
-        year: bazi.year,
-        month: bazi.month,
-        date: bazi.date,
-        hour: bazi.hour,
+        bazi,
         wuxing: WanNianLi.getResult(lunar).wuxing,
-        xunshou: xunShou + "-" + xunShouList[xunShou]
+        xunshou: xunShou + '-' + xunShouList[xunShou],
     });
 });
 
-app.get('/getJieQi', function (req, res) {
+app.get('/getJieQi', function(req, res) {
     res.send(SolarTerm(now));
 });
 
-app.get('/getInfo', function (req, res) {
+app.get('/getInfo', function(req, res) {
     res.send(qimen);
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
 
@@ -125,20 +134,18 @@ app.listen(3000, function () {
  * @param {*现在时间} DateGL
  */
 function SolarTerm(DateGL) {
-    var SolarTermStr = new Array(
-        "小寒-yang-285", "大寒-yang-396", "立春-yang-852", "雨水-yang-963",
-        "惊蛰-yang-174", "春分-yang-396", "清明-yang-417", "谷雨-yang-528",
-        "立夏-yang-417", "小满-yang-528", "芒种-yang-639", "夏至-yin-936",
-        "小暑-yin-825", "大暑-yin-714", "立秋-yin-258", "处暑-yin-147",
-        "白露-yin-936", "秋分-yin-714", "寒露-yin-693", "霜降-yin-582",
-        "立冬-yin-693", "小雪-yin-582", "大雪-yin-471", "冬至-yang-174");
-    var DifferenceInMonth = new Array(
-        1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
+    const SolarTermStr = ['小寒-yang-285', '大寒-yang-396', '立春-yang-852', '雨水-yang-963',
+        '惊蛰-yang-174', '春分-yang-396', '清明-yang-417', '谷雨-yang-528',
+        '立夏-yang-417', '小满-yang-528', '芒种-yang-639', '夏至-yin-936',
+        '小暑-yin-825', '大暑-yin-714', '立秋-yin-258', '处暑-yin-147',
+        '白露-yin-936', '秋分-yin-714', '寒露-yin-693', '霜降-yin-582',
+        '立冬-yin-693', '小雪-yin-582', '大雪-yin-471', '冬至-yang-174'];
+    const DifferenceInMonth = [1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
         1321560, 1333035, 1342770, 1350855, 1356420, 1359045,
         1358580, 1355055, 1348695, 1340040, 1329630, 1318455,
-        1306935, 1297380, 1286865, 1277730, 1274550, 1271556);
-    var DifferenceInYear = 31556926;
-    var BeginTime = new Date(1901 / 1 / 1);
+        1306935, 1297380, 1286865, 1277730, 1274550, 1271556];
+    const DifferenceInYear = 31556926;
+    const BeginTime = new Date(1901 / 1 / 1);
     BeginTime.setTime(947120460000);
     for (; DateGL.getFullYear() < BeginTime.getFullYear();) {
         BeginTime.setTime(BeginTime.getTime() - DifferenceInYear * 1000);
@@ -158,10 +165,10 @@ function SolarTerm(DateGL) {
         M == 23 ? M = 0 : M++;
     }
 
-    var yuan = "";
-    var res = ""
-    var diff = Math.ceil((BeginTime.getTime() - DateGL.getTime()) / 86400000);
-    console.log(diff)
+    let yuan = '';
+    let res = '';
+    let diff = Math.ceil((BeginTime.getTime() - DateGL.getTime()) / 86400000);
+
     /**
      * 15 天一个循环
      * 1-5 上元 0
@@ -177,17 +184,17 @@ function SolarTerm(DateGL) {
     if (diff > 10) {
         yuan = 2;
     }
-    var data = SolarTermStr[M - 1].split("-");
-    if (data[1] == "yin") {
-        res = "阴遁" + data[2].split("")[yuan] + " 局";
+    let data = SolarTermStr[M - 1].split('-');
+    if (data[1] == 'yin') {
+        res = '阴遁' + data[2].split('')[yuan] + ' 局';
     } else {
-        res = "阳遁" + data[2].split("")[yuan] + " 局";
+        res = '阳遁' + data[2].split('')[yuan] + ' 局';
     }
 
-    var tmp = {
+    let tmp = {
         msg: res,
-        '地盘地支': getDiPanDiZhi(data[1] + "-" + data[2].split("")[yuan])
-    }
+        '地盘地支': getDiPanDiZhi(data[1] + '-' + data[2].split('')[yuan]),
+    };
     return JSON.stringify(tmp);
 }
 
@@ -196,13 +203,14 @@ function SolarTerm(DateGL) {
  * yin/yang-N
  * @param {String} info
  */
-function getDiPanDiZhi(info) {
-    var result = {};
-    var type = info.split("-")[0];
-    const num = info.split("-")[1];
-    if (type == "yin") {
+const getDiPanDiZhi = (info) => {
+    let i;
+    const result = {};
+    const type = info.split('-')[0];
+    const num = info.split('-')[1];
+    if (type === 'yin') {
         // 阴遁 逆着排
-        for (var i = 0; i < 9; i++) {
+        for (i = 0; i < 9; i++) {
             if (i < num) {
                 result[num - i] = diPanDiZhiList[i];
 
@@ -217,15 +225,15 @@ function getDiPanDiZhi(info) {
         result[num] = diPanDiZhiList[0];
 
         // 2. cas 1 - num
-        for (var i = 1; i < num; i++) {
+        for (i = 1; i < num; i++) {
             result[i] = diPanDiZhiList[9 - num + i];
         }
 
         // 3. cas num - 9
-        for (var i = num; i < 9; i++) {
+        for (i = num; i < 9; i++) {
             result[parseInt(i) + 1] = diPanDiZhiList[i - num + 1];
         }
 
     }
     return result;
-}
+};
