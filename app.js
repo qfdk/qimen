150fe050b5339a049d452050b5277b858b169761
qfdk <qfdk2010@gmail.com>
2021-10-12 22:22:43 +0200

    fix : 修改布局

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const WanNianLi = require('./lib/wuxing');
+const HuangLi = require('./lib/wuxing');
 const calendar = require('./lib/calendar');
-const diPanDiZhiList = ['戊', '己', '庚', '辛', '壬', '癸', '丁', '丙', '乙'];
 
-const {
-    xunShouList,
-    diPan,
-    men,
-    xing,
-    xingList
-} = require('./lib/constants');
+const {xunShouList, diPanDiZhiList, diPan, men, xing, xingList} = require('./lib/constants');
 
 // 现时
 let now;
@@ -25,49 +18,45 @@ app.engine('html', require('ejs').renderFile);
 // public files
 app.use(express.static(path.join(__dirname, 'public')));
 
-const getTimeDetail = (now) => {
+const timeDetail = (now) => {
     const year = now.getFullYear();
     const month = now.getMonth() + 1;
     const date = now.getDate();
     const hour = now.getHours();
-    return { year, month, date, hour };
+    return {year, month, date, hour};
 };
 
 // index
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     now = new Date();
-    const { year, month, date, hour } = getTimeDetail(now);
+    const {year, month, date, hour} = timeDetail(now);
     // 农历
     const lunar = calendar.solar2lunar(year, month, date);
     lunar.hour = hour;
 
     // 四柱
-    const bazi = WanNianLi.getResult(lunar).bazi;
+    const bazi = HuangLi.getResult(lunar).bazi;
     // 甲子列表
-    const jiazi = WanNianLi.getResult(lunar).jiazi;
+    const jiazi = HuangLi.getResult(lunar).jiazi.filter(e => e);
     // 旬首
-    const xunShou = jiazi[jiazi.indexOf(bazi.hour) - (jiazi.indexOf(bazi.hour) % 10) + 1];
-    qimen['xunShou'] = xunShouList[xunShou];
-    qimen['shiGan'] = bazi.hour;
-    console.log(qimen);
-    console.log(bazi);
+    const xunShou = jiazi[jiazi.indexOf(bazi.hour) - (jiazi.indexOf(bazi.hour) % 10)];
+    qimen['旬首'] = xunShouList[xunShou];
+    qimen['时干'] = bazi.hour;
+    set天盘星();
+    SolarTerm(now);
     res.render('index', {
         time: year + '/' + month + '/' + date + ' ' + hour + ':' + now.getMinutes(),
         bazi,
-        wuxing: WanNianLi.getResult(lunar).wuxing,
-        xunshou: xunShou + '-' + xunShouList[xunShou],
+        wuxing: HuangLi.getResult(lunar).wuxing,
+        xunshou: xunShou + '-' + xunShouList[xunShou]
     });
 });
 
-app.get('/getJieQi', function (req, res) {
-    res.send(SolarTerm(now));
+app.get('/getInfo', function(req, res) {
+    res.json(qimen);
 });
 
-app.get('/getInfo', function (req, res) {
-    res.send(qimen);
-});
-
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
 
@@ -84,41 +73,43 @@ app.listen(3000, function () {
  * 霜降、小雪五八二，大雪四七一。
  * @param {*现在时间} DateGL
  */
-function SolarTerm(DateGL) {
-    const SolarTermStr = ['小寒-yang-285', '大寒-yang-396', '立春-yang-852', '雨水-yang-963',
+function SolarTerm(inputDate) {
+    const SolarTermStr = [
+        '小寒-yang-285', '大寒-yang-396', '立春-yang-852', '雨水-yang-963',
         '惊蛰-yang-174', '春分-yang-396', '清明-yang-417', '谷雨-yang-528',
         '立夏-yang-417', '小满-yang-528', '芒种-yang-639', '夏至-yin-936',
         '小暑-yin-825', '大暑-yin-714', '立秋-yin-258', '处暑-yin-147',
         '白露-yin-936', '秋分-yin-714', '寒露-yin-693', '霜降-yin-582',
         '立冬-yin-693', '小雪-yin-582', '大雪-yin-471', '冬至-yang-174'];
-    const DifferenceInMonth = [1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
+    const DifferenceInMonth = [
+        1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
         1321560, 1333035, 1342770, 1350855, 1356420, 1359045,
         1358580, 1355055, 1348695, 1340040, 1329630, 1318455,
         1306935, 1297380, 1286865, 1277730, 1274550, 1271556];
     const DifferenceInYear = 31556926;
     const BeginTime = new Date(1901 / 1 / 1);
     BeginTime.setTime(947120460000);
-    for (; DateGL.getFullYear() < BeginTime.getFullYear();) {
+    for (; inputDate.getFullYear() < BeginTime.getFullYear();) {
         BeginTime.setTime(BeginTime.getTime() - DifferenceInYear * 1000);
     }
-    for (; DateGL.getFullYear() > BeginTime.getFullYear();) {
+    for (; inputDate.getFullYear() > BeginTime.getFullYear();) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInYear * 1000);
     }
-    for (var M = 0; DateGL.getMonth() > BeginTime.getMonth(); M++) {
+    for (var M = 0; inputDate.getMonth() > BeginTime.getMonth(); M++) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
     }
-    if (DateGL.getDate() > BeginTime.getDate()) {
+    if (inputDate.getDate() > BeginTime.getDate()) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
         M++;
     }
-    if (DateGL.getDate() > BeginTime.getDate()) {
+    if (inputDate.getDate() > BeginTime.getDate()) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
         M == 23 ? M = 0 : M++;
     }
 
     let yuan = '';
     let res = '';
-    let diff = Math.ceil((BeginTime.getTime() - DateGL.getTime()) / 86400000);
+    let diff = Math.ceil((BeginTime.getTime() - inputDate.getTime()) / 86400000);
 
     /**
      * 15 天一个循环
@@ -141,12 +132,8 @@ function SolarTerm(DateGL) {
     } else {
         res = '阳遁' + data[2].split('')[yuan] + ' 局';
     }
-
-    let tmp = {
-        msg: res,
-        '地盘地支': getDiPanDiZhi(data[1] + '-' + data[2].split('')[yuan]),
-    };
-    return JSON.stringify(tmp);
+    qimen['地盘地支'] = getDiPanDiZhi(data[1] + '-' + data[2].split('')[yuan]);
+    qimen['局'] = res;
 }
 
 /**
@@ -188,3 +175,29 @@ const getDiPanDiZhi = (info) => {
     }
     return result;
 };
+
+/**
+ * 拿到星
+ */
+function set天盘星() {
+    var zhiFuXing = '';
+    for (var dizhi in qimen['地盘地支']) {
+        if (qimen['地盘地支'][dizhi] == qimen['旬首']) {
+            zhiFuXing = xing[dizhi];
+        }
+    }
+    var shiGan = qimen['时干'].split('')[0];
+    var newLuoGong = '';
+    for (var t in qimen['地盘地支']) {
+        if (qimen['地盘地支'][t] == shiGan) {
+            newLuoGong = t;
+        }
+    }
+    var old = xingList.indexOf(zhiFuXing);
+    var offset = newLuoGong - old;
+    var tianPanXing = {};
+    for (var i = 0; i < xingList.length; i++) {
+        tianPanXing[(i + offset + 8) % 8 + 1] = xingList[i];
+    }
+    qimen['天盘星'] = tianPanXing;
+}
--- public/js/app.js
+++ public/js/app.js
@@ -2,84 +2,79 @@
  * Created by qfdk on 17/11/05.
  */
 var diPan = {
-    "6": "乾",
-    "1": "坎",
-    "8": "艮",
-    "3": "震",
-    "4": "巽",
-    "9": "离",
-    "2": "坤",
-    "7": "兑"
-}
+    '6': '乾',
+    '1': '坎',
+    '8': '艮',
+    '3': '震',
+    '4': '巽',
+    '9': '离',
+    '2': '坤',
+    '7': '兑'
+};
 var men = {
-    "6": "开门",
-    "1": "休门",
-    "8": "生门",
-    "3": "伤门",
-    "4": "杜门",
-    "9": "景门",
-    "2": "死门",
-    "7": "惊门"
-}
-
-var xing = {
-    "6": "天心",
-    "1": "天蓬",
-    "8": "天任",
-    "3": "天冲",
-    "4": "天辅",
-    "9": "天英",
-    "2": "天芮",
-    "7": "天柱"
-}
-var xingType = {
-    "天心": "jin",
-    "天蓬": "shui",
-    "天任": "tu",
-    "天冲": "mu",
-    "天辅": "mu",
-    "天英": "huo",
-    "天芮": "tu",
-    "天柱": "jin",
-}
-var dizhiType = {
-    "戊": 'tu',
-    "己": 'tu',
-    "庚": 'jin',
-    "辛": 'jin',
-    "壬": 'shui',
-    "癸": 'shui',
-    "丁": 'huo',
-    "丙": 'huo',
-    "乙": 'mu'
+    '6': '开门',
+    '1': '休门',
+    '8': '生门',
+    '3': '伤门',
+    '4': '杜门',
+    '9': '景门',
+    '2': '死门',
+    '7': '惊门'
 };
 
-$(document).ready(function () {
-    $.get('/getJieQi', function (data) {
-        var json = JSON.parse(data);
-        $('#jieQi').text(json.msg);
-        var 地盘地支List = json.地盘地支;
+var xing = {
+    '6': '天心',
+    '1': '天蓬',
+    '8': '天任',
+    '3': '天冲',
+    '4': '天辅',
+    '9': '天英',
+    '2': '天芮',
+    '7': '天柱'
+};
+var xingType = {
+    '天心': 'jin',
+    '天蓬': 'shui',
+    '天任': 'tu',
+    '天冲': 'mu',
+    '天辅': 'mu',
+    '天英': 'huo',
+    '天芮': 'tu',
+    '天柱': 'jin'
+};
+var dizhiType = {
+    '戊': 'tu',
+    '己': 'tu',
+    '庚': 'jin',
+    '辛': 'jin',
+    '壬': 'shui',
+    '癸': 'shui',
+    '丁': 'huo',
+    '丙': 'huo',
+    '乙': 'mu'
+};
+
+$(document).ready(function() {
+    $.get('/getInfo', function(data) {
+        $('#jieQi').text(data['局']);
+        var dipanDizhiList = data['地盘地支'];
+        for (var index in data['天盘星']) {
+            $('#xing' + index).text(data['天盘星'][index]);
+            $('#xing' + index).addClass(xingType[data['天盘星'][index]]);
+            if (data['天盘星'][index] === '天芮') {
+                $('#tianqin' + index).text('天禽');
+            }
+        }
         /**
          * 地盘-地支
          */
-        for (var dz in 地盘地支List) {
-            $('#diPanDiZhi' + dz).text(地盘地支List[dz]);
-            $('#diPanDiZhi' + dz).addClass(dizhiType[地盘地支List[dz]]);
-        }
-
-    });
-    $.get('/getInfo', function (data) {
-        var index = $('.diPanDiZhi').text().split("").indexOf(data.xunShou);
-        console.log(index)
-        console.log("值符", xing[index]);
-        console.log("值使", men[index]);
-        for (var index in data.tianPanXing) {
-            $('#xing' + index).text(data.tianPanXing[index]);
-            $('#xing' + index).addClass(xingType[data.tianPanXing[index]]);
-            // if (data.tianPanXing[index] == "天芮") {
-            //     $('#tianqin' + index).text("天禽");
-            //     $('#wu' + index).text("　戊");
-            // }
+        for (var dz in dipanDizhiList) {
+            if (data['旬首'] === dipanDizhiList[dz]) {
+                console.log('值符', xing[dz]);
+                console.log('值使', men[dz]);
+            }
+            $('#diPanDiZhi' + dz).text(dipanDizhiList[dz]);
+            $('#diPanDiZhi' + dz).addClass(dizhiType[dipanDizhiList[dz]]);
         }
     });
 });
