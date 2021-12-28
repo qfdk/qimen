76da265d3cd4bc637bd398f318428fa1b8aa291f
qfdk <qfdk2010@gmail.com>
2021-12-28 22:17:16 +0100

    fix: 修复错误 todo 检查算法

--- app.js
+++ app.js
@@ -57,6 +57,57 @@ app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
 
+function SolarTerm(DateGL) {
+    var SolarTermStr = new Array(
+        "小寒", "大寒", "立春", "雨水", "惊蛰", "春分",
+        "清明", "谷雨", "立夏", "小满", "芒种", "夏至",
+        "小暑", "大暑", "立秋", "处暑", "白露", "秋分",
+        "寒露", "霜降", "立冬", "小雪", "大雪", "冬至");
+    var DifferenceInMonth = new Array(
+        1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
+        1321560, 1333035, 1342770, 1350855, 1356420, 1359045,
+        1358580, 1355055, 1348695, 1340040, 1329630, 1318455,
+        1306935, 1297380, 1286865, 1277730, 1274550, 1271556);
+    var DifferenceInYear = 31556926;
+    var BeginTime = new Date(1901 / 1 / 1);
+    BeginTime.setTime(947120460000);
+    for (; DateGL.getFullYear() < BeginTime.getFullYear();) {
+        BeginTime.setTime(BeginTime.getTime() - DifferenceInYear * 1000);
+    }
+    for (; DateGL.getFullYear() > BeginTime.getFullYear();) {
+        BeginTime.setTime(BeginTime.getTime() + DifferenceInYear * 1000);
+    }
+    for (var M = 0; DateGL.getMonth() > BeginTime.getMonth(); M++) {
+        BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
+    }
+    if (DateGL.getDate() > BeginTime.getDate()) {
+        BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
+        M++;
+    }
+    if (DateGL.getDate() > BeginTime.getDate()) {
+        BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
+        M == 23 ? M = 0 : M++;
+    }
+    console.log(M);
+    var JQ = "二十四节气";
+    if (DateGL.getDate() == BeginTime.getDate()) {
+        JQ += "    今日 <font color='#598F03'><b>" + SolarTermStr[M] + "</b></font>";
+    } else if (DateGL.getDate() == BeginTime.getDate() - 1) {
+        JQ += "　 明日 <font color='#598F03'><b>" + SolarTermStr[M] + "</b></font>";
+    } else if (DateGL.getDate() == BeginTime.getDate() - 2) {
+        JQ += "　 后日 <font color='#598F03'><b>" + SolarTermStr[M] + "</b></font>";
+    } else {
+        JQ = " 二十四节气";
+        if (DateGL.getMonth() == BeginTime.getMonth()) {
+            JQ += " 本月";
+        } else {
+            JQ += " 下月";
+        }
+        JQ += BeginTime.getDate() + "日" + "<font color='#598F03'><b>" + SolarTermStr[M] + "</b></font>";
+    }
+    return JQ;
+}
+
 /**
  * 阳遁：
  * 冬至、惊蛰一七四，小寒二八五，
@@ -78,6 +129,7 @@ const set局数 = (inputDate) => {
         '小暑-yin-825', '大暑-yin-714', '立秋-yin-258', '处暑-yin-147',
         '白露-yin-936', '秋分-yin-714', '寒露-yin-693', '霜降-yin-582',
         '立冬-yin-693', '小雪-yin-582', '大雪-yin-471', '冬至-yang-174'];
+
     const DifferenceInMonth = [
         1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
         1321560, 1333035, 1342770, 1350855, 1356420, 1359045,
@@ -95,10 +147,7 @@ const set局数 = (inputDate) => {
     for (var M = 0; inputDate.getMonth() > BeginTime.getMonth(); M++) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
     }
-    if (inputDate.getDate() > BeginTime.getDate()) {
-        BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
-        M++;
-    }
+
     if (inputDate.getDate() > BeginTime.getDate()) {
         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
         M == 23 ? M = 0 : M++;
@@ -123,7 +172,8 @@ const set局数 = (inputDate) => {
     if (diff > 10) {
         yuan = 2;
     }
-    let data = JuMapping[M - 1].split('-');
+
+    let data = JuMapping[(M - 1) < 0 ? M - 1 + 24 : M].split('-');
     if (data[1] == 'yin') {
         ju = '阴遁' + data[2].split('')[yuan] + ' 局';
     } else {
