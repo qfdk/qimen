391f3a3eb541c281b8e3e5d7aa76d49c8aa9fcf8
qfdk <qfdk2010@gmail.com>
2025-04-30 00:11:54 +0200

    fix: 新样式..

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const {Lunar, Solar} = require('lunar-javascript');
-const {xunShouMap, diPanDiZhiList, xing, xingList} = require('./lib/constants');
 
-const qimen = {};
+// 导入奇门遁甲计算模块
+const qimen = require('./lib/qimen');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,192 +14,178 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
 app.disable('view cache');
 
-/**
- * 获取四柱
- * @param date 时间 new Date()
- */
-const getSiZhu = (LUNAR) => {
-    return {
-        year: LUNAR.getYearInGanZhi(),
-        month: LUNAR.getMonthInGanZhi(),
-        day: LUNAR.getDayInGanZhi(),
-        time: LUNAR.getTimeInGanZhi()
-    };
-};
-
-const getXunshou = (LUNAR) => {
-    return LUNAR.getTimeXun();
-};
-
-// index
+// 首页 - 实时排盘
 app.get('/', (req, res) => {
-    const LUNAR = Lunar.fromDate(new Date());
-    // 获取当时四柱
-    const sizhu = getSiZhu(LUNAR);
-
-    // 旬首
-    const xunShou = getXunshou(LUNAR);
-    qimen['旬首'] = xunShouMap[xunShou];
-    qimen['时干'] = sizhu.time;
-
-    // set局数(now);
-    // 计算局数 考虑传参数进来
-    qimen['局'] = "阳-5";
-    qimen['地盘地支'] = get地盘地支(qimen['局']);
-    set天盘星();
-    res.render('index', {
-        time: Solar.fromDate(new Date()).toFullString(),
-        sizhu,
-        xunshou: xunShou + '-' + xunShouMap[xunShou]
-    });
-});
-
-app.get('/getInfo', (req, res) => {
-    res.json(qimen);
-});
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
-
-/**
- * 阳遁：
- * 冬至、惊蛰一七四，小寒二八五，
- * 大寒、春分三九六，雨水九六三，
- * 清明、立夏四一七，立春八五二，
- * 谷雨、小满五二八，芒种六三九。
- * 阴遁：
- * 夏至、白露九三六，小暑八二五，
- * 大暑、秋分七一四，立秋二五八，
- * 寒露、立冬六九三，处暑一四七，
- * 霜降、小雪五八二，大雪四七一。
- * 茅山法 15日 按照节气来
- * @param {*现在时间} inputDate
- */
-// const set局数 = (inputDate) => {
-//     const JuMapping = [
-//         '小寒-yang-285', '大寒-yang-396', '立春-yang-852', '雨水-yang-963',
-//         '惊蛰-yang-174', '春分-yang-396', '清明-yang-417', '谷雨-yang-528',
-//         '立夏-yang-417', '小满-yang-528', '芒种-yang-639', '夏至-yin-936',
-//         '小暑-yin-825', '大暑-yin-714', '立秋-yin-258', '处暑-yin-147',
-//         '白露-yin-936', '秋分-yin-714', '寒露-yin-693', '霜降-yin-582',
-//         '立冬-yin-693', '小雪-yin-582', '大雪-yin-471', '冬至-yang-174'];
-//
-//     const DifferenceInMonth = [
-//         1272060, 1275495, 1281180, 1289445, 1299225, 1310355,
-//         1321560, 1333035, 1342770, 1350855, 1356420, 1359045,
-//         1358580, 1355055, 1348695, 1340040, 1329630, 1318455,
-//         1306935, 1297380, 1286865, 1277730, 1274550, 1271556];
-//     const DifferenceInYear = 31556926;
-//     const BeginTime = new Date(1901 / 1 / 1);
-//     BeginTime.setTime(947120460000);
-//     for (; inputDate.getFullYear() < BeginTime.getFullYear();) {
-//         BeginTime.setTime(BeginTime.getTime() - DifferenceInYear * 1000);
-//     }
-//     for (; inputDate.getFullYear() > BeginTime.getFullYear();) {
-//         BeginTime.setTime(BeginTime.getTime() + DifferenceInYear * 1000);
-//     }
-//     for (var M = 0; inputDate.getMonth() > BeginTime.getMonth(); M++) {
-//         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
-//     }
-//
-//     if (inputDate.getDate() > BeginTime.getDate()) {
-//         BeginTime.setTime(BeginTime.getTime() + DifferenceInMonth[M] * 1000);
-//         M == 23 ? M = 0 : M++;
-//     }
-//
-//     let yuan = '';
-//     let ju = '';
-//     let diff = Math.ceil((inputDate.getTime() - BeginTime.getTime()) / 86400000);
-//
-//     /**
-//      * 15 天一个循环
-//      * 1-5 上元 0
-//      * 5-10 中元 1
-//      * 11-15 下元 2
-//      */
-//     if (diff <= 5) {
-//         yuan = 0;
-//     }
-//     if (diff > 5 && diff <= 10) {
-//         yuan = 1;
-//     }
-//     if (diff > 10) {
-//         yuan = 2;
-//     }
-//
-//     let data = JuMapping[(M - 1) < 0 ? M - 1 + 24 : M].split('-');
-//     if (data[1] == 'yin') {
-//         ju = '阴遁' + data[2].split('')[yuan] + ' 局';
-//     } else {
-//         ju = '阳遁' + data[2].split('')[yuan] + ' 局';
-//     }
-//     qimen['地盘地支'] = get地盘地支(data[1] + '-' + data[2].split('')[yuan]);
-//     qimen['局'] = ju;
-// };
-
-/**
- * 阴阳遁-局数
- * yin/yang-N
- * @param {String} info
- */
-const get地盘地支 = (info) => {
-    let i;
-    const result = {};
-    const type = info.split('-')[0];
-    const num = info.split('-')[1];
-    if (type === 'yin') {
-        // 阴遁 逆着排
-        for (i = 0; i < 9; i++) {
-            if (i < num) {
-                result[num - i] = diPanDiZhiList[i];
-
-            } else {
-                result[num - i + 9] = diPanDiZhiList[i];
+    // 获取当前时间
+    const date = new Date();
+    
+    // 计算奇门盘
+    const options = {
+        type: '四柱',
+        method: '时家',
+        purpose: '综合',
+        location: '默认位置'
+    };
+    
+    try {
+        const qimenPan = qimen.calculate(date, options);
+        
+        // 初始化缺失的属性，确保模板不会报错
+        if (!qimenPan.jiuGongAnalysis) {
+            qimenPan.jiuGongAnalysis = {};
+        }
+        
+        // 确保每个宫位都有基本属性
+        for (let i = 1; i <= 9; i++) {
+            if (!qimenPan.jiuGongAnalysis[i]) {
+                qimenPan.jiuGongAnalysis[i] = {
+                    direction: '',
+                    gongName: '',
+                    jiXiong: 'ping'
+                }
             }
         }
+        
+        // 传递常量给视图
+        res.locals.JIU_GONG = qimen.JIU_GONG;
+        res.locals.JIU_XING = qimen.JIU_XING;
+        res.locals.BA_MEN = qimen.BA_MEN;
+        res.locals.BA_SHEN = qimen.BA_SHEN;
+        
+        // 渲染页面
+        res.render('index', { qimen: qimenPan });
+    } catch (error) {
+        console.error('排盘错误:', error);
+        // 返回错误页面
+        res.status(500).send('排盘错误: ' + error.message);
+    }
+});
+
+// 自定义排盘
+app.get('/custom', (req, res) => {
+    // 获取请求参数
+    const type = req.query.type || '四柱';
+    const method = req.query.method || '时家';
+    const dateStr = req.query.date;
+    const timeStr = req.query.time;
+    const location = req.query.location || '默认位置';
+    const purpose = req.query.purpose || '综合';
+    
+    // 解析日期时间
+    let date;
+    if (dateStr && timeStr) {
+        date = new Date(`${dateStr}T${timeStr}`);
     } else {
-        // 阳遁 顺着排
-
-        // 1. cas
-        result[num] = diPanDiZhiList[0];
-
-        // 2. cas 1 - num
-        for (i = 1; i < num; i++) {
-            result[i] = diPanDiZhiList[9 - num + i];
-        }
-
-        // 3. cas num - 9
-        for (i = num; i < 9; i++) {
-            result[parseInt(i) + 1] = diPanDiZhiList[i - num + 1];
-        }
-
+        date = new Date();
     }
-    return result;
-};
+    
+    // 检查日期是否有效
+    if (isNaN(date.getTime())) {
+        return res.status(400).send('无效的日期时间');
+    }
+    
+    try {
+        // 计算奇门盘
+        const options = {
+            type,
+            method,
+            purpose,
+            location
+        };
+        
+        const qimenPan = qimen.calculate(date, options);
+        
+        // 初始化缺失的属性，确保模板不会报错
+        if (!qimenPan.jiuGongAnalysis) {
+            qimenPan.jiuGongAnalysis = {};
+        }
+        
+        // 确保每个宫位都有基本属性
+        for (let i = 1; i <= 9; i++) {
+            if (!qimenPan.jiuGongAnalysis[i]) {
+                qimenPan.jiuGongAnalysis[i] = {
+                    direction: '',
+                    gongName: '',
+                    jiXiong: 'ping'
+                }
+            }
+        }
+        
+        // 传递常量给视图
+        res.locals.JIU_GONG = qimen.JIU_GONG;
+        res.locals.JIU_XING = qimen.JIU_XING;
+        res.locals.BA_MEN = qimen.BA_MEN;
+        res.locals.BA_SHEN = qimen.BA_SHEN;
+        
+        // 渲染页面
+        res.render('index', { qimen: qimenPan });
+    } catch (error) {
+        console.error('自定义排盘错误:', error);
+        // 返回错误页面
+        res.status(500).send('排盘错误: ' + error.message);
+    }
+});
 
-/**
- * 拿到星
- */
-const set天盘星 = () => {
-    let zhiFuXing = '';
-    for (const dizhi in qimen['地盘地支']) {
-        if (qimen['地盘地支'][dizhi] === qimen['旬首']) {
-            zhiFuXing = xing[dizhi];
+// API接口 - 获取奇门排盘数据
+app.get('/api/qimen', (req, res) => {
+    // 获取请求参数
+    const type = req.query.type || '四柱';
+    const method = req.query.method || '时家';
+    const dateStr = req.query.date;
+    const timeStr = req.query.time;
+    const location = req.query.location || '默认位置';
+    const purpose = req.query.purpose || '综合';
+    
+    // 解析日期时间
+    let date;
+    if (dateStr && timeStr) {
+        date = new Date(`${dateStr}T${timeStr}`);
+    } else {
+        date = new Date();
+    }
+    
+    // 检查日期是否有效
+    if (isNaN(date.getTime())) {
+        return res.status(400).json({ error: '无效的日期时间' });
+    }
+    
+    try {
+        // 计算奇门盘
+        const options = {
+            type,
+            method,
+            purpose,
+            location
+        };
+        
+        const qimenPan = qimen.calculate(date, options);
+        
+        // 初始化缺失的属性，确保模板不会报错
+        if (!qimenPan.jiuGongAnalysis) {
+            qimenPan.jiuGongAnalysis = {};
         }
-    }
-    const shiGan = qimen['时干'].split('')[0];
-    let newLuoGong = '';
-    for (const t in qimen['地盘地支']) {
-        if (qimen['地盘地支'][t] === shiGan) {
-            newLuoGong = t;
+        
+        // 确保每个宫位都有基本属性
+        for (let i = 1; i <= 9; i++) {
+            if (!qimenPan.jiuGongAnalysis[i]) {
+                qimenPan.jiuGongAnalysis[i] = {
+                    direction: '',
+                    gongName: '',
+                    jiXiong: 'ping'
+                }
+            }
         }
+        
+        // 返回JSON数据
+        res.json(qimenPan);
+    } catch (error) {
+        console.error('API排盘错误:', error);
+        res.status(500).json({ error: '排盘错误', message: error.message });
     }
-    const old = xingList.indexOf(zhiFuXing);
-    const offset = newLuoGong - old;
-    const tianPanXing = {};
-    for (let i = 0; i < xingList.length; i++) {
-        tianPanXing[(i + offset + 8) % 8 + 1] = xingList[i];
-    }
-    qimen['天盘星'] = tianPanXing;
-};
+});
+
+// 启动服务器
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`奇门遁甲排盘系统正在运行，请访问 http://localhost:${port}`);
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,82 +1,15 @@
-/**
- * Created by qfdk on 17/11/05.
- */
-var diPan = {
-    '6': '乾',
-    '1': '坎',
-    '8': '艮',
-    '3': '震',
-    '4': '巽',
-    '9': '离',
-    '2': '坤',
-    '7': '兑'
-};
-var men = {
-    '6': '开门',
-    '1': '休门',
-    '8': '生门',
-    '3': '伤门',
-    '4': '杜门',
-    '9': '景门',
-    '2': '死门',
-    '7': '惊门'
-};
-
-var xing = {
-    '6': '天心',
-    '1': '天蓬',
-    '8': '天任',
-    '3': '天冲',
-    '4': '天辅',
-    '9': '天英',
-    '2': '天芮',
-    '7': '天柱'
-};
-var xingType = {
-    '天心': 'jin',
-    '天蓬': 'shui',
-    '天任': 'tu',
-    '天冲': 'mu',
-    '天辅': 'mu',
-    '天英': 'huo',
-    '天芮': 'tu',
-    '天柱': 'jin'
-};
-var dizhiType = {
-    '戊': 'tu',
-    '己': 'tu',
-    '庚': 'jin',
-    '辛': 'jin',
-    '壬': 'shui',
-    '癸': 'shui',
-    '丁': 'huo',
-    '丙': 'huo',
-    '乙': 'mu'
-};
-
 $(document).ready(function() {
-    $.get('/getInfo', function(data) {
-        $('#jieQi').text(data['局']);
-        var dipanDizhiList = data['地盘地支'];
-        for (var index in data['天盘星']) {
-            $('#xing' + index).text(data['天盘星'][index]);
-            $('#xing' + index).addClass(xingType[data['天盘星'][index]]);
-            if (data['天盘星'][index] === '天芮') {
-                $('#tianqin' + index).text('天禽');
-            }
-        }
-        /**
-         * 地盘-地支
-         */
-        for (var dz in dipanDizhiList) {
-
-            if (data['旬首'] === dipanDizhiList[dz]) {
-                console.log('值符', xing[dz]);
-                console.log('值使', men[dz]);
-            }
-
-            $('#diPanDiZhi' + dz).text(dipanDizhiList[dz]);
-            $('#diPanDiZhi' + dz).addClass(dizhiType[dipanDizhiList[dz]]);
-        }
+    // 自定义排盘表单提交
+    $('#submitCustomPan').click(function() {
+        $('#customPanForm').submit();
     });
+
+    // 确保九宫格始终保持正方形比例
+    function maintainAspectRatio() {
+        var gridWidth = $('.pan-grid').width();
+        $('.gong').css('height', gridWidth / 3 + 'px');
+    }
+
+    maintainAspectRatio();
+    $(window).resize(maintainAspectRatio);
 });
