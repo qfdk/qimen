8b80086d5d024093784799dc28bda21a33358720
qfdk <qfdk2010@gmail.com>
2025-12-01 20:56:38 +0100

    fix: 优化样式和修正八神五行属性
    - 移除悬停动画效果和tooltip
    - 统一字号为18px
    - 修正八神五行属性颜色
    - 金属性颜色改为#FF8109
    - 自定义排盘表单默认填充当前时间
    - 简化CSS代码

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    // 设置自定义排盘表单的默认日期和时间
+    var now = new Date();
+    var dateStr = now.getFullYear() + '-' +
+        String(now.getMonth() + 1).padStart(2, '0') + '-' +
+        String(now.getDate()).padStart(2, '0');
+    var timeStr = String(now.getHours()).padStart(2, '0') + ':' +
+        String(now.getMinutes()).padStart(2, '0');
+
+    $('#date').val(dateStr);
+    $('#time').val(timeStr);
+
     // 自定义排盘表单提交
     $('#submitCustomPan').click(function() {
         $('#customPanForm').submit();
